390efda8672c00ef1aafb7de0ce83e671fdba3e1
magnificientA <102520567+magnificientA@users.noreply.github.com>
2022-06-14 15:04:26 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='header-class'>
+       Basic React Application
+     </h1>
+    </div>
+  );
+}
+
+export default App;
